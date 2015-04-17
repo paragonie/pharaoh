be048c1d5150a3a8a60fe6f46e7cdbfadb9c94f7
Scott <scott@paragonie.com>
2015-04-16 23:42:26 -0400

    Diff the PHAR Stub too!

--- src/Pharaoh/PharDiff.php
+++ src/Pharaoh/PharDiff.php
@@ -30,6 +30,88 @@ class PharDiff
     
     public function listChecksums($algo)
     {
-        // Iterate both directories; hash all the files
+        list($pharA, $pharB) = $this->hashChildren(
+            $algo,
+            $this->phars[0]->tmp,
+            $this->phars[1]->tmp
+        );
+        
+        $count = count($pharA);
+        for ($i = 0; $i < $count; ++$i) {
+            
+        }
+    }
+    
+    /**
+     * Get hashes of all of the files in the two arrays.
+     * 
+     * @param type $algo
+     * @param type $dirA
+     * @param type $dirB
+     * @return type
+     */
+    public function hashChildren($algo, $dirA, $dirB)
+    {
+        $filesA = $this->listAllFiles($dirA);
+        $filesB = $this->listAllFiles($dirB);
+        $numFiles = \max(\count($filesA), \count($filesB));
+        
+        // Array of two empty arrays
+        $hashes = [[], []];
+        for ($i = 0; $i < $numFiles; ++$i) {
+            if (isset($filesA)) {
+                $a = \preg_replace('#^'.\preg_quote($dirA, '#').'#', '', $filesA[$i]);
+            }
+            if (isset($filesB)) {
+                $b = \preg_replace('#^'.\preg_quote($dirB, '#').'#', '', $filesB[$i]);
+            }
+            
+            if (isset($filesA[$i])) {
+                // A exists
+                $hashes[0][$a] = \hash_file($algo, $filesA[$i]);
+            } elseif (isset($filesB[$i])) {
+                // A doesn't exist, B does
+                $hashes[0][$b] = null;
+            }
+            
+            if (isset($filesB[$i])) {
+                // B exists
+                $hashes[1][$b] = \hash_file($algo, $filesB[$i]);
+            } elseif (isset($filesA[$i])) {
+                // B doesn't exist, A does
+                $hashes[1][$a] = null;
+            }
+        }
+        return $hashes;
+    }
+    
+    
+    /**
+     * List all the files in a directory (and subdirectories)
+     *
+     * @param string $folder - start searching here
+     * @param string $extension - extensions to match
+     * @return array
+     */
+    private function listAllFiles($folder, $extension = '*')
+    {
+        $dir = new \RecursiveDirectoryIterator($folder);
+        $ite = new \RecursiveIteratorIterator($dir);
+        if ($extension === '*') {
+            $pattern = '/.*/';
+        } else {
+            $pattern = '/.*\.' . $extension . '$/';
+        }
+        $files = new \RegexIterator($ite, $pattern, \RegexIterator::GET_MATCH);
+        $fileList = [];
+        foreach($files as $file) {
+            $fileList = \array_merge($fileList, $file);
+        }
+        foreach ($fileList as $i => $file) {
+            if (\preg_match('#/\.{1,2}$#', $file)) {
+                unset($fileList[$i]);
+            }
+        }
+        return \array_values($fileList);
     }
 }
--- src/Pharaoh/Pharaoh.php
+++ src/Pharaoh/Pharaoh.php
@@ -5,18 +5,30 @@ class Pharaoh
 {
     public $phar;
     public $tmp;
+    public static $stubfile;
     
     public function __construct($file, $alias = null)
     {
         if (!\is_readable($file)) {
             throw new PharError($file.' cannot be read');
         }
+        if (\ini_get('phar.readonly') == '1') {
+            throw new PharError("Pharaoh cannot be used if phar.readonly is enabled in php.ini\n");
+        }
+        
+        // Set the static variable here
+        if (empty(self::$stubfile)) {
+            self::$stubfile = \bin2hex(
+                \mcrypt_create_iv(9, MCRYPT_DEV_URANDOM)
+            ).'.pharstub';
+        }
+        
         $this->phar = new \Phar($file);
         
         if (empty($alias)) {
             // We have to give every one a different alias, or it pukes.
-            $alias = \base64_encode(
-                \mcrypt_create_iv(9, MCRYPT_DEV_URANDOM)
+            $alias = \bin2hex(
+                \mcrypt_create_iv(8, MCRYPT_DEV_URANDOM)
             ).'.phar';
         }
         $this->phar->setAlias($alias);
@@ -28,5 +40,11 @@ class Pharaoh
         
         // Let's extract to our temporary directory
         $this->phar->extractTo($this->tmp);
+        
+        // Also extract the stub
+        \file_put_contents(
+            $this->tmp.'/'.self::$stubfile,
+            $this->phar->getStub()
+        );
     }
 }
